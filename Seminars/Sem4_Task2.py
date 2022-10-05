d743af7f5f0369b82cb5897bcf0a7c31e8cc4df9
Marina <mv.nikolaeva1507@gmail.com>
2022-10-05 12:30:31 +0300

    correct Sem4_Tasks 1,2,3

--- Seminars/Sem4_Task2.py
+++ Seminars/Sem4_Task2.py
@@ -2,25 +2,25 @@
 # с помощью математических формул нахождения корней квадратного уравнения
 # с помощью дополнительных библиотек Python (например, numpy.roots)
 
-# import numpy as np
-# p = [1, 5, 6]
-# roots = np.roots(p)
-# print(roots)
+import numpy as np
+p = [1, 5, 6]
+roots = np.roots(p)
+print(roots)
 
-a = float(input("Введите число A: "))
-b = float(input("Введите число B: "))
-с = float(input("Введите число C: "))
-discr = (b ** 2) - (4 * a * с)
-print(discr)
-if discr > 0:
-    x1 = round((-b + ((discr) ** 0.5)) / (2 * a), 2)
-    x2 = round((-b - ((discr) ** 0.5)) / (2 * a), 2)
-    print(x1, x2)
-elif discr == 0:
-    x1 = round(-b / (2 * a), 2)
-    print(x1)
-elif discr < 0:
-    print('Нет корней')
+# a = float(input("Введите число A: "))
+# b = float(input("Введите число B: "))
+# с = float(input("Введите число C: "))
+# discr = (b ** 2) - (4 * a * с)
+# print(discr)
+# if discr > 0:
+#     x1 = round((-b + ((discr) ** 0.5)) / (2 * a), 2)
+#     x2 = round((-b - ((discr) ** 0.5)) / (2 * a), 2)
+#     print(x1, x2)
+# elif discr == 0:
+#     x1 = round(-b / (2 * a), 2)
+#     print(x1)
+# elif discr < 0:
+#     print('Нет корней')
 
 
 # a, b, c = list(map(int, input('Введите три числа через пробел: ').split())) # Сами распакуются
