7c6face98d1d4815e99fd996299a027b046f857f
Marina <mv.nikolaeva1507@gmail.com>
2022-10-14 17:02:07 +0300

    add sem7

---
+++ Seminars/sem7/calc_op.py
@@ -0,0 +1,27 @@
+# x = 0
+# y = 0
+
+# def init(a, b):
+#     global x
+#     global y
+#     x = a
+#     y = b
+
+# def do_it(): 
+#     return x + y
+
+
+def calc_sum(a, b):
+    return a + b
+
+
+def calc_mult(a, b):
+    return a * b
+
+
+def calc_sub(a, b):
+    return a - b
+
+ 
+def calc_div(a, b): 
+    return a / b
